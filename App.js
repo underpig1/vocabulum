00c74483c6dce65f90ab8b4c80d845861baad892
underpig1 <chasepgraham@icloud.com>
2023-08-02 11:01:44 -1000

    update app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useWindowDimensions } from "react-native";
+import WebView from "react-native-webview";
+const index = require("./app/mobile.html");
+
+export default function App() {
+  const { width } = useWindowDimensions();
+  return (
+    <WebView
+      contentWidth={width}
+      source={index}
+    />
+  );
+}
